a93bfdecd1a8e9d42eef89a23429daa2cec3a057
Usernamenuh <muhammadenuh4@gmail.com>
2025-06-02 23:37:25 +0700

    M

--- resources/views/catatan/show.blade.php
+++ resources/views/catatan/show.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>Detail Catatan</h3>
+        </div>
+    </div>
+    <div class="card-body">
+        <h5>Kegiatan: {{ $catatan->kegiatan }}</h5>
+        <p>Keterangan: {{ $catatan->keterangan }}</p>
+        <p>Jumlah Main HP: {{ $catatan->jumlah_mainhp }}</p>
+    </div>
+    <div class="card-footer">
+        <a href="{{ route('catatan.index') }}" class="btn btn-secondary">Kembali</a>
+    </div>
 </div>
+@endsection
